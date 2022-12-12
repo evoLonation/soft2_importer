dd477df73d04019e7874825f8c3ea20eae7b35da
evoLonation <1838940019@qq.com>
2022-12-12 16:34:11 +0800

    修复类型转换bug

--- common/errors.go
+++ common/errors.go
@@ -9,15 +9,21 @@ import (
 func HandleResponseError(res *esapi.Response) bool {
 	raw := map[string]interface{}{}
 	if res.IsError() {
-		str := "http from ES responses error! \n"
+		log.Printf("http from ES responses error! \n")
 		if err := json.NewDecoder(res.Body).Decode(&raw); err != nil {
-			log.Panic(str, "parse error response body error\n", err.Error())
+			log.Panic("parse error response body error\n", err.Error())
 		} else {
-			log.Printf("ES http response Errors:\nstatus:%s\n%s\n%s\n%s\n",
-				res.Status(),
-				raw["error"].(map[string]interface{})["type"].(string),
-				raw["error"].(map[string]interface{})["reason"].(string),
-			)
+			_, success := raw["error"].(map[string]interface{})
+			if success {
+				log.Printf("ES http response Errors:\nstatus:%s\n%s\n%s\n%s\n",
+					res.Status(),
+					raw["error"].(map[string]interface{})["type"].(string),
+					raw["error"].(map[string]interface{})["reason"].(string),
+				)
+			} else {
+				log.Printf("ES http response Errors:\n")
+				log.Println(raw["error"])
+			}
 			return false
 		}
 	}
